1484085383ac11c897a1ac9b9ad0df51d34789bd
pgromiec <pgromiec@gmail.com>
2025-08-03 07:42:00 +0100

    fix: enabled JWT authentication for protected endpoints - fixes #15

--- src/main/java/com/bidding/crew/security/SecurityConfig.java
+++ src/main/java/com/bidding/crew/security/SecurityConfig.java
@@ -35,15 +35,14 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/auth/**",
-                                        "/swagger-ui/**",
-                                        "/v3/api-docs/**").permitAll()
-//                        .requestMatchers("/api/v0/**").hasRole("USER")
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
-//                        .anyRequest().authenticated()
-                                .anyRequest().permitAll()
+                        .requestMatchers("/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v0/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+//                        .anyRequest().permitAll()
                 )
-                //.httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(authenticationProvider())
